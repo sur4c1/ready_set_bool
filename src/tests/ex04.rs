9766d3103e5610d1b8e0035ea093a08e62051e43
sur4c1 <94838879+sur4c1@users.noreply.github.com>
2023-12-07 19:05:15 +0100

    pruot

---
+++ src/tests/ex04.rs
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex04.rs                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 18:20:58 by bguyot            #+#    #+#             */
+/*   Updated: 2023/12/07 18:31:00 by bguyot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+use	crate::exs::print_truth_table::print_truth_table;
+
+pub fn	ex04()
+{
+	print_truth_table("0");
+	print_truth_table("1");
+	print_truth_table("A");
+	print_truth_table("B");
+	print_truth_table("");
+	print_truth_table("AB&");
+	print_truth_table("AB&C|");
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -2,3 +2,4 @@ pub mod ex00;
 pub mod ex01;
 pub mod ex02;
 pub mod ex03;
+pub mod ex04;
